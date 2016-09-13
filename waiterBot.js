07b6cb7022e5be357c4d2799f360fda5a079aadc
Christian Fei <crifei93@gmail.com>
2016-09-13 22:03:55 +0200

    respond right away, boilerplate delayedReply

--- waiterBot.js
+++ waiterBot.js
@@ -2,29 +2,57 @@
 const botBuilder = require('claudia-bot-builder')
 const slackDelayedReply = botBuilder.slackDelayedReply
 
+const aws = require('aws-sdk')
+const lambda = new aws.Lambda()
+
 const waiterBotCommandHandler = require('./lib/waiterBotCommandHandler')
 const slackResponseFormatter = require('./lib/slackResponseFormatter')
 
-const api = botBuilder((request) => {
+const api = botBuilder((request, apiRequest) => {
   const response = waiterBotCommandHandler(request.originalRequest)
-  console.log('-- responding with', response)
-  return slackResponseFormatter(response)
+  return response.then(slackResponseFormatter)
+
+  return delayReply(request, apiRequest)
+  .then(() => {
+    console.log('-- successfully handled command', request, response)
+    return response.then(slackResponseFormatter)
+  })
+  .catch((err) => {
+    console.log('-- failed to handle command', request, err)
+    return slackResponseFormatter("I messed up, sorry.")
+  })
 })
 
 api.intercept((event) => {
-  if (!event.slackEvent) // if this is a normal web request, let it run
+  if (!event.delayedReply) {
     return event
+  }
 
   console.log('-- event', event)
   return false
-  // const message = event.slackEvent
-  // const seconds = parseInt(message.text, 10)
+  // const request = event.delayedReply
+  // const seconds = parseInt(request.text, 10)
 
-  // return slackDelayedReply(message, {
+  // return slackDelayedReply(request, {
   //   text: `${seconds} seconds passed.`,
   //   response_type: 'in_channel'
   // })
   // .then(() => false) // prevent normal execution
 })
+
+function delayReply(request, apiRequest) {
+  return new Promise((resolve, reject) => {
+    lambda.invoke({
+      FunctionName: apiRequest.lambdaContext.functionName,
+      InvocationType: 'Event',
+      Payload: JSON.stringify({
+        delayedReply: request
+      }),
+      Qualifier: apiRequest.lambdaContext.functionVersion
+    }, (err, done) => {
+      return err ? reject(err) : resolve()
+    })
+  })
+}
 
 module.exports = api
