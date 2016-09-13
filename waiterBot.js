9d892022aa78f46fda03e2cbba01a007503585b5
Christian Fei <crifei93@gmail.com>
2016-09-13 19:43:29 +0200

    format slack response

--- waiterBot.js
+++ waiterBot.js
@@ -1,9 +1,11 @@
 'use strict'
 const botBuilder = require('claudia-bot-builder')
+
 const waiterBotCommandHandler = require('./lib/waiterBotCommandHandler')
+const slackResponseFormatter = require('./lib/slackResponseFormatter')
 
 module.exports = botBuilder((request) => {
   const response = waiterBotCommandHandler(request.originalRequest)
   console.log('-- responding with', response)
-  return response
+  return response.then(slackResponseFormatter)
 })
