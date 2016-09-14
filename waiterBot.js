bb42b92088ab969e3a9da55ec8a3cf3b240d7000
Christian Fei <crifei93@gmail.com>
2016-09-14 14:38:39 +0200

    handle delayed response for echo command

--- waiterBot.js
+++ waiterBot.js
@@ -3,71 +3,52 @@ const botBuilder = require('claudia-bot-builder')
 const CommandHandler = require('./lib/commands/CommandHandler')
 const slackResponseFormatter = require('./lib/slackResponseFormatter')
 
+const slackDelayedReply = botBuilder.slackDelayedReply
+const aws = require('aws-sdk')
+const lambda = new aws.Lambda()
+
 const log = (payload) => {console.log('-- responding', payload); return payload}
 const api = botBuilder((request, apiRequest) => {
   console.log('-- starting to handle', request)
   const response = CommandHandler.handle(request.originalRequest)
-  return response.then(log).then(slackResponseFormatter)
+  return registerDelayedResponse(request, apiRequest)
+  .then(() => {
+    return response.then(log).then(slackResponseFormatter)
+  })
+  .catch((err) => {
+    console.log('-- failed to handle command', request, err)
+    return slackResponseFormatter("I messed up, sorry.")
+  })
 })
 
 module.exports = api
 
+api.intercept(event => {
+  console.log('-- event', event)
+  if (!event.delayedReply) {
+    return event
+  }
 
+  const request = event.delayedReply
+  const result = CommandHandler.handleDelayedResponse(request.originalRequest)
 
+  return result.then(response => {
+    return slackDelayedReply(request, slackResponseFormatter(response))
+  })
+  .then(() => false) // prevent normal execution
+})
 
-
-
-
-
-
-
-
-
-
-
-
-// const slackDelayedReply = botBuilder.slackDelayedReply
-// const aws = require('aws-sdk')
-// const lambda = new aws.Lambda()
-
-  // return delayReply(request, apiRequest)
-  // .then(() => {
-  //   console.log('-- successfully handled command', request, response)
-  //   return response.then(slackResponseFormatter)
-  // })
-  // .catch((err) => {
-  //   console.log('-- failed to handle command', request, err)
-  //   return slackResponseFormatter("I messed up, sorry.")
-  // })
-
-// api.intercept((event) => {
-//   if (!event.delayedReply) {
-//     return event
-//   }
-
-//   console.log('-- event', event)
-//   return false
-//   // const request = event.delayedReply
-//   // const seconds = parseInt(request.text, 10)
-
-//   // return slackDelayedReply(request, {
-//   //   text: `${seconds} seconds passed.`,
-//   //   response_type: 'in_channel'
-//   // })
-//   // .then(() => false) // prevent normal execution
-// })
-
-// function delayReply(request, apiRequest) {
-//   return new Promise((resolve, reject) => {
-//     lambda.invoke({
-//       FunctionName: apiRequest.lambdaContext.functionName,
-//       InvocationType: 'Event',
-//       Payload: JSON.stringify({
-//         delayedReply: request
-//       }),
-//       Qualifier: apiRequest.lambdaContext.functionVersion
-//     }, (err, done) => {
-//       return err ? reject(err) : resolve()
-//     })
-//   })
-// }
+function registerDelayedResponse(request, apiRequest) {
+  return new Promise((resolve, reject) => {
+    lambda.invoke({
+      FunctionName: apiRequest.lambdaContext.functionName,
+      InvocationType: 'Event',
+      Payload: JSON.stringify({
+        delayedReply: request
+      }),
+      Qualifier: apiRequest.lambdaContext.functionVersion
+    }, (err, done) => {
+      return err ? reject(err) : resolve()
+    })
+  })
+}
