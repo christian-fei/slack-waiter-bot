0386c6dafc2910a219d207aa53ca0652580163f2
Christian Fei <crifei93@gmail.com>
2016-09-16 15:54:01 +0200

    extract botBuilder api to botBuilderRequestHandler

--- waiterBot.js
+++ waiterBot.js
@@ -1,54 +1,9 @@
 'use strict'
 const botBuilder = require('claudia-bot-builder')
-const CommandHandler = require('./lib/commands/CommandHandler')
-const slackResponseFormatter = require('./lib/slackResponseFormatter')
+const BotRequestHandler = require('./lib/BotRequestHandler')
 
-const slackDelayedReply = botBuilder.slackDelayedReply
-const aws = require('aws-sdk')
-const lambda = new aws.Lambda()
+const botRequestHandler = new BotRequestHandler(botBuilder.slackDelayedReply)
 
-const log = (payload) => {console.log('-- responding', payload); return payload}
-const api = botBuilder((request, apiRequest) => {
-  console.log('-- starting to handle', request)
-  const response = CommandHandler.handle(request.originalRequest)
-  return registerDelayedResponse(request, apiRequest)
-  .then(() => {
-    return response.then(log).then(slackResponseFormatter)
-  })
-  .catch((err) => {
-    console.log('-- failed to handle command', request, err)
-    return slackResponseFormatter("I messed up, sorry.")
-  })
-})
-
+const api = botBuilder(botRequestHandler.handler.bind(botRequestHandler))
+api.intercept(botRequestHandler.intercept.bind(botRequestHandler))
 module.exports = api
-
-api.intercept(event => {
-  console.log('-- event', event)
-  if (!event.delayedReply) {
-    return event
-  }
-
-  const request = event.delayedReply
-  const result = CommandHandler.handleDelayedResponse(request.originalRequest)
-
-  return result.then(response => {
-    return slackDelayedReply(request, slackResponseFormatter(response))
-  })
-  .then(() => false) // prevent normal execution
-})
-
-function registerDelayedResponse(request, apiRequest) {
-  return new Promise((resolve, reject) => {
-    lambda.invoke({
-      FunctionName: apiRequest.lambdaContext.functionName,
-      InvocationType: 'Event',
-      Payload: JSON.stringify({
-        delayedReply: request
-      }),
-      Qualifier: apiRequest.lambdaContext.functionVersion
-    }, (err, done) => {
-      return err ? reject(err) : resolve()
-    })
-  })
-}
