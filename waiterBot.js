d08ddfe2669502aaa469010a179635c118f4ddcd
Christian Fei <crifei93@gmail.com>
2016-09-17 20:42:05 +0200

    BotRequestHandle to module

--- waiterBot.js
+++ waiterBot.js
@@ -2,8 +2,10 @@
 const botBuilder = require('claudia-bot-builder')
 const BotRequestHandler = require('./lib/BotRequestHandler')
 
-const botRequestHandler = new BotRequestHandler(botBuilder.slackDelayedReply)
-
-const api = botBuilder(botRequestHandler.handler.bind(botRequestHandler))
-api.intercept(botRequestHandler.intercept.bind(botRequestHandler))
+const api = botBuilder((request, apiRequest) => {
+  return BotRequestHandler.handler(request, apiRequest)
+})
+api.intercept((event) => {
+  return BotRequestHandler.intercept(event, botBuilder.slackDelayedReply)
+})
 module.exports = api
