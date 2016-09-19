fa04621faa6852adaad5c8a07be01322623687b8
Christian Fei <crifei93@gmail.com>
2016-09-19 14:16:09 +0200

    remove obsolete deps

--- test.helper.js
+++ test.helper.js
@@ -1,9 +1,7 @@
 'use strict'
-const sinon = require('sinon')
 const chai = require('chai')
 const chaiAsPromised = require("chai-as-promised")
 chai.use(chaiAsPromised)
 
 global.chai = chai
 global.expect = chai.expect
-global.sinon = sinon
